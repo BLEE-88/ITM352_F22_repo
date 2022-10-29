26a241e686fc2b993183b599948b6d28bc1e2e24
BLEE-88 <112213087+BLEE-88@users.noreply.github.com>
2022-10-28 22:38:05 -1000

    Add files via upload

---
+++ Lee_Brendan_Assignment1/public/product_data.js
@@ -0,0 +1,41 @@
+
+products = [
+    {  
+      "flower":"Plumeria",  
+      "price": 35.00,  
+      "image": "./images/Plumeria.jpg",
+      "quantity_available": 8
+    },
+    {  
+      "flower":"Hibiscus",  
+      "price": 20.00,  
+      "image": "./images/Hibiscus.jpg",
+      "quantity_available": 16
+    },
+    {  
+      "flower":"Purple Orchid",  
+      "price": 40.00,  
+      "image": "./images/Orchid.jpg",
+      "quantity_available": 15
+    },
+    {  
+      "flower":"Heliconia",  
+      "price": 25.00,  
+      "image": "./images/Heliconia.jpg",
+      "quantity_available": 14
+    }, 
+    {  
+      "flower":"Anthurium",  
+      "price": 30.00,  
+      "image": "./images/Anthurium.jpg",
+      "quantity_available": 11
+    }, 
+    {  
+      "flower":"Gardenia",  
+      "price": 30.00,  
+      "image": "./images/Gardenia.jpg",
+      "quantity_available": 9
+    } 
+    ];
+
+quantities = [4, -6 , 0 , 2.2 , 5];
