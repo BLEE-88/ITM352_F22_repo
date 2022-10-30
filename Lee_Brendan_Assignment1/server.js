637fdf09cbe2323009c19688b833f5e87afed01d
BLEE-88 <112213087+BLEE-88@users.noreply.github.com>
2022-10-29 21:55:53 -1000

    Add files via upload

--- Lee_Brendan_Assignment1/server.js
+++ Lee_Brendan_Assignment1/server.js
@@ -1,4 +1,4 @@
-var products_array = require(__dirname + '/products.json');
+var products = require(__dirname + '/products.json');
 
 var express = require('express');
 var app = express();
@@ -27,13 +27,33 @@ function isNonNegIntString (queryString, returnErrors=false) {
 }
 }
 
-app.get("/invoice", function (request, response, next) {
+var products = require(__dirname + '/product_data.json');
+products.forEach( (prod,i) => {prod.total_sold = 0}); 
+
+app.get("/product_data.js", function (request, response, next) {
    response.type('.js');
    var products_str = `var products = ${JSON.stringify(products)};`;
    response.send(products_str);
 });
 
+app.post("/process_form", function (request, response) {
+    //response.send(request.body)
+    var q = request.body['text1'];
+    if (typeof q != 'undefined') {
+        if (isNonNegativeInteger(q)) {  // We have a valid quantity
+            
+            let brand = products[0]['name'];
+            let brand_price = products[0]['price'];
+            products[0].total_sold += Number(q);
 
+            response.send(`<H1>Invoice</H1><BR>Thank you for purchasing <B>${q}</B> ${brand} at ${brand_price} each for a total of ${brand_price * q}`);
+        } else {
+            response.send(`${q} is not a valid quantity -- hit the back button`);
+        }
+    } else {
+        response.send("Enter some quantities!");
+    }
+});
 // route all other GET requests to files in public 
 app.use(express.static(__dirname + '/public'));
 
