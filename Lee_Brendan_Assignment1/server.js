86a61e28caeb9e902c2fbcf8e325b0ece0832d09
BLEE-88 <112213087+BLEE-88@users.noreply.github.com>
2022-10-30 15:22:43 -1000

    Add files via upload

--- Lee_Brendan_Assignment1/server.js
+++ Lee_Brendan_Assignment1/server.js
@@ -1,5 +1,4 @@
-var products = require(__dirname + '/products.json');
-
+var products_array = require(__dirname + '/products.json');
 var express = require('express');
 var app = express();
 
@@ -11,47 +10,45 @@ app.all('*', function (request, response, next) {
    next();
 });
 
-// process purchase request (validate quantities, check quantity available)
-function isNonNegIntString (queryString, returnErrors=false) {
+// process purchase request (validate quantities, check quantity available) From Lab and Wods
+function isNonNegativeInteger(queryString, returnErrors = false) {
     errors = []; // assume no errors at first
-        if(Number(queryString) != queryString) errors.push('Not a number!'); // Check if string is a number value
-        if(queryString < 0) errors.push('Negative value!'); // Check if it is non-negative
-        if(parseInt(queryString) != queryString) errors.push('Not an integer!'); // Check that it is an integer
-    
-        if (returnErrors) {
-    return errors;
-} else if (errors.length == 0) {
-    return true;
-} else {
-    return false;
-}
-}
+    if (Number(queryString) != queryString) errors.push('Not a number!'); // Check if string is a number value
+    if (queryString < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if (parseInt(queryString) != queryString) errors.push('Not an integer!'); // Check that it is an integer
 
-app.get("/product_data.js", function (request, response, next) {
-   response.type('.js');
-   var products_str = `var products = ${JSON.stringify(products)};`;
-   response.send(products_str);
-});
-
-app.post('/process_form', function (request, response, next) {
-    let brand = products[0]['flower'];
-    let brand_price = products[0]['price'];
-
-    console.log(request.body);
-    var q = request.body['quantity_textbox'];
-    if (typeof q != 'undefined') {
-        if(isNonNegInt(q)) { 
-            products[0].total_sold += Number(q);
-            response.redirect('./invoice.html?quantity='+q);
-        } else {
-            response.redirect('./invoice.html?error=Invalid%20Quantity&quantity_textbox=' + q);
-        }
+    if (returnErrors) {
+        return errors;
+    } else if (errors.length == 0) {
+        return true;
     } else {
-        response.send(`Hey! You need to pick some stuff!`);
+        return false;
     }
-
-    next();
-});
+}
+// From Assignment 1 Example 
+function process_quantity_form (POST, response) {
+    if (typeof POST['PurchaseButton'] != 'undefined') {
+       var contents = fs.readFileSync('./views/display_quantity_template.view', 'utf8');
+       receipt = '';
+        for(i in products) { 
+            let q = POST[`quantity${i}`];
+            let item = products[i]['flower'];
+            let item_price = products[i]['price'];
+            if (isNonNegIntString(q)) {
+            receipt += eval('`' + contents + '`'); // render template string
+            } else {
+            receipt += `<h3><font color="red">${q} is not a valid quantity for ${flower}!</font></h3>`;
+            }
+        }
+      response.send(receipt);
+      response.end();
+    }
+ }
+ app.get("/products.js", function (request, response, next) {
+    response.type('.js');
+    var products_str = `var products = ${JSON.stringify(products)};`;
+    response.send(products_str);
+ });
 
 // route all other GET requests to files in public 
 app.use(express.static(__dirname + '/public'));
