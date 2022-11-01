61d916cd09b5d153752db4bafa97004d52f135c0
BLEE-88 <112213087+BLEE-88@users.noreply.github.com>
2022-11-01 12:59:47 -1000

    Add files via upload

--- Lee_Brendan_Assignment1/server.js
+++ Lee_Brendan_Assignment1/server.js
@@ -1,28 +1,20 @@
-var products_array = require(__dirname + '/products.json');
 var express = require('express');
 var app = express();
+var path = require('path');
 
-// Routing 
 
-// monitor all requests
-app.all('*', function (request, response, next) {
-   console.log(request.method + ' to ' + request.path);
-   next();
-});
-
-// process purchase request (validate quantities, check quantity available) From Lab and Wods
-app.get("/products.js", function (request, response, next) {
-    response.type('.js');
-    var products_str = `var products = ${JSON.stringify(products)};`;
-    response.send(products_str);
- });
+app.use(express.static(__dirname + '/public'));
+app.use('/css',express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true })); 
 
 function isNonNegativeInteger(queryString, returnErrors = false) {
     errors = []; // assume no errors at first
-    if (Number(queryString) != queryString) errors.push('Not a number!'); // Check if string is a number value
-    if (queryString < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if (parseInt(queryString) != queryString) errors.push('Not an integer!'); // Check that it is an integer
-
+    if (Number(queryString) != queryString) {
+        errors.push('Not a number!'); // Check if string is a number value
+    } else {
+        if (queryString < 0) errors.push('Negative value!'); // Check if it is non-negative
+        if (parseInt(queryString) != queryString) errors.push('Not an integer!'); // Check that it is an integer
+    }
     if (returnErrors) {
         return errors;
     } else if (errors.length == 0) {
@@ -31,11 +23,26 @@ function isNonNegativeInteger(queryString, returnErrors = false) {
         return false;
     }
 }
-app.post("./process_form", function (request, response) {
-    // Process the form and redirect to the invoice page if everything is valid
+
+var products = require(__dirname + '/product_data.json');
+products.forEach((prod, i) => { prod.total_sold = 0 });
+
+app.get("/product_data.js", function (request, response, next) {
+    response.type('.js');
+    var products_str = `var products = ${JSON.stringify(products)};`;
+    response.send(products_str);
+});
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path);
+    next();
+});
+
+app.post("/process_form", function (request, response) {
+    // Process the form by redirecting to the receipt page if everything is valid.
     let valid = true;
     let ordered = "";
- 
+
     for (i = 0; i < products.length; i++) {  // Iterate over all text boxes in the form.
         var flower = "quantity" + i;
         var q = request.body[flower];
@@ -60,11 +67,6 @@ app.post("./process_form", function (request, response) {
         // If everything is good, redirect to the invoice page.
         response.redirect('invoice.new.html?' + ordered);
     }
- 
  });
 
-// route all other GET requests to files in public 
-app.use(express.static(__dirname + '/public'));
-
-// start server
 app.listen(8080, () => console.log(`listening on port 8080`));
