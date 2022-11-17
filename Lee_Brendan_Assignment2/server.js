e24578863c6bd032a34d0b78f20f8b5c760fa617
BLEE-88 <112213087+BLEE-88@users.noreply.github.com>
2022-11-16 19:28:21 -1000

    Add files via upload

--- Lee_Brendan_Assignment2/server.js
+++ Lee_Brendan_Assignment2/server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var fs = require('fs');
+var errors = {};
+
+// Global array for IR5
+let activeusers = {};
 
 app.use(express.static(__dirname + '/public'));
 app.use('/css',express.static(__dirname + '/public'));
@@ -65,11 +69,11 @@ app.post("/Purchase", function (request, response) {
     }
     if (!valid) {
         // If we found an error, redirect back to the products_display page.
-        response.redirect('products_display.html?error=Invalid%20Quantity');
+        response.redirect('products_display.html');
 
     } else {
-        // If everything is good, redirect to the invoice page.
-        response.redirect('/login?' + ordered + params.toString());
+        // If everything is good, redirect to the login page.
+        response.redirect('/login?' + ordered);
     }
  });
 
@@ -83,6 +87,7 @@ var fname = "user_data.json";
      var users = JSON.parse(data);
      console.log(users);
  };
+// Created login form on server with signup button
 
  app.get("/products.js", function (request, response, next) {
     response.type('.js');
@@ -136,25 +141,25 @@ body {
 response.send(str);
 console.log("He: " + params);
 });
-
+//Login validation
  app.post("/login", function (request, response) {
     let params = new URLSearchParams(request.query);
     // Process login form POST and redirect to logged in page if ok, back to login page if not
-    the_username = request.body['username'].toLowerCase();
+    user_name = request.body['username'].toLowerCase();
     the_password = request.body['password'];
-    if (typeof users[the_username] != 'undefined') {
-        if (users[the_username].password == the_password) {
-            response.redirect('./invoice.html?' + params.toString());
+    if (typeof users[user_name] != 'undefined') {
+        if (users[user_name].password == the_password) {
+            response.redirect('./invoice.html?' + user_name + "&" + params.toString()); // If username and pass match go to invoice
         } else {
-            response.send(`Wrong password!`);
+            response.send(`Wrong password!`); //Wrong password if not
         }
         return;
     }
-    response.send(`${the_username} does not exist!`);
+    response.send(`${user_name} does not exist!`); //If username matches one already existing
 });
 
-// From Assignment 2 Examples
 
+// Created register form on server
 app.get("/register", function (request, response) {
     console.log(request.params.toString());
 // Give a simple login form
@@ -189,18 +194,25 @@ border-width: 1px;
             <form id="register_form" action="register" method="POST">
             <label>Username</label>
             <input type="text" name="username" size="30" placeholder="enter username"></br>
+            ${ (typeof errors['user_exists'] != 'undefined')?errors['user_exists']:''}
+            ${ (typeof errors['no_username'] != 'undefined')?errors['no_username']:''}
             <br>
             <label>Email</label>
             <input type="email" name="email" size="30" placeholder="enter email"><br/>
+            ${ (typeof errors['invalid_email'] != 'undefined')?errors['invalid_email']:''}
             <br>
             <label>Full Name:</label>
             <input type="text" name="fullname" size="30" placeholder="enter full name"><br />
+            ${ (typeof errors['invalid_name'] != 'undefined')?errors['invalid_name']:''}
             <br>
             <label>Password:</label>
             <input type="password" name="newpassword" size="30" placeholder="enter password"><br />
+            ${ (typeof errors['passtooshort'] != 'undefined')?errors['passtooshort']:''}
+            ${ (typeof errors['passtoolong'] != 'undefined')?errors['passtoolong']:''}
             <br>
             <label>Confirm Password:</label>
             <input type="password" name="repeat_password" size="30" placeholder="re-enter password"></br>
+            ${ (typeof errors['passnotmatch'] != 'undefined')?errors['passnotmatch']:''}
             <br>
             <input type="submit" value="Register" name="register_user" id="register">
             </div>
@@ -209,47 +221,47 @@ border-width: 1px;
     `;
 response.send(str);
 });
-
+// Registration validation
 app.post("/register", function (request, response) {
         // process a simple register form
         let params = new URLSearchParams(request.query);
-        notgood = false;
+        good = false;
         username = request.body.username.toLowerCase();
         email = request.body.email.toLowerCase();
         password = request.body.newpassword.length
     
-        if(typeof users[username] != 'undefined') {
-            response.send(`Hey! ${username} is already registered!`);
-            notgood = true;
+        if(typeof users[username] != 'undefined') { //If user already exists
+            errors['user_exists'] = `User already exists!`;
+            good = true;
         }
-        if(request.body.username == '') {
-            response.send(`You need to enter a username!`);
-            notgood = true;
+        if(request.body.username == '') { //Username box is filled in
+            errors['no_username'] = `You need to select a username!`;
+            good = true;
         }
         if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) { //tests if email matches validation
         } else {
-            response.send(`Invalid email format!`);
-            notgood = true;
+            errors['invalid_email'] = `Invalid email!`;
+            good = true;
         }
-        if (/^[a-z ,.'-]+$/i.test(request.body.fullname)) {
+        if (/^[a-z ,.'-]+$/i.test(request.body.fullname)) { //Name matches valid format
         } else {
-            response.send(`Please use a real name!`);
-            notgood = true;
+            errors['invalid_name'] = `Please use a real name!`;
+            good = true;
+        } 
+        if (password < 10) { //Password is not long enough
+            errors['passtooshort'] = `Your password is too short!`;
+            good = true;
         }
-        if (password < 10) {
-            response.send(`Password too short!`);
-            notgood = true;
+        if (password > 16) { //Password is too long
+            errors['passtoolong'] = `Your password is too long!`;
+            good = true;
         }
-        if (password > 16) {
-            response.send(`Password too long!`);
-            notgood = true;
-        }
-        if (request.body.newpassword != request.body.repeat_password) {
-            response.send(`Passwords do not match!`);
-            notgood = true;
+        if (request.body.newpassword != request.body.repeat_password) { //Not matching passwords
+            errors['passnotmatch'] = `Password does not match!`;
+            good = true;
         }
 
-        if (!notgood) { //no errors
+        if (!good) { //If there are no errors add it to user_data
             let POST = request.body;
         
             let user_name = POST["username"];
@@ -265,22 +277,23 @@ app.post("/register", function (request, response) {
         
                 let data = JSON.stringify(users);
                 fs.writeFileSync(fname, data, 'utf-8');
-                response.redirect("invoice.html?" + params.toString());
+                response.redirect("invoice.html?" + user_name + "&" + params.toString());
             } else {
-                response.redirect('/login.html?error = User Already Exists!');
+                response.redirect('/login?error = User Already Exists!');
 
             }
         }
+        else {
+            response.redirect("/register?" + "&" + params.toString());
+        }
      });
-     app.post("/invoice", function (request, response) {
+//Attempt at IR5 making invoice a form and then adding loop to get username into array
+app.post("/invoice", function (request, response) {
         the_username = request.body['username'].toLowerCase();
         the_password = request.body['password'];
-        if (typeof users[the_username] != 'undefined') {
-            if (users[the_username].password == the_password) {
-                loggedin.push({})
-     }
-    }
+        
 });
+
 
 
 
